b2216d0eca419cd821fd940236c7c4a11e201a91
yoaku005 <yoaku005@d.umn.edu>
2018-11-27 11:19:04 -0600

    Initial commit
    set up frame work for lab

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,33 @@
 package segmentedfilesystem;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+
 public class Main {
-    
-    public static void main(String[] args) {
-        
+    ArrayList<OurPacket> ourPackets = new ArrayList<OurPacket>();
+
+    public static void main(String[] args) throws Exception{
+
+        //Create client Socket
+        DatagramSocket clientSocket = new DatagramSocket(6014);
+
+        //Package request data
+        byte[] receiveData = new byte[8];
+        String sendString = "polo";
+        byte[] sendData = sendString.getBytes("UTF-8");
+        DatagramPacket recievePacket = new DatagramPacket(receiveData,receiveData.length);
+
+        //Collect packets
+        while(true){
+            clientSocket.receive(recievePacket);
+
+            OurPacket ourPacket = new OurPacket(recievePacket);
+        }
+    }
+
+    public void splitFilesApart(){
+        //Split ourPackets based on file
     }
 
 }
---
+++ src/segmentedfilesystem/OurPacket.java
@@ -0,0 +1,32 @@
+package segmentedfilesystem;
+
+import java.net.DatagramPacket;
+
+public class OurPacket {
+    boolean isHeader;
+    int packetNumber;
+    byte[] contents;
+    int fileID;
+
+    public OurPacket(DatagramPacket packet){
+        packet.getLength();
+        byte[] data = packet.getData();
+
+        this.isHeader = data[0]%2 == 0;
+        this.fileID = data[1];
+
+        if(isHeader){
+
+        } else {
+            this.packetNumber = data[2];
+            for(int i = 3; i < data.length; i ++){
+                contents[i-3] = data[i];
+            }
+        }
+    }
+
+
+    public int compareTo(OurPacket packet){
+        return packetNumber - packet.packetNumber;
+    }
+}
